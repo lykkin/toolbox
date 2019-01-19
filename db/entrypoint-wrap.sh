cd16d0da3de8eb311b1191c83812541c98a4e6ce
Bryan Clement <bclement@newrelic.com>
2019-01-18 21:57:39 -0800

    added parent id

--- db/entrypoint-wrap.sh
+++ db/entrypoint-wrap.sh
@@ -2,7 +2,7 @@
 
 # Create default keyspace for single node cluster
 KS="CREATE KEYSPACE IF NOT EXISTS span_collector WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-TS="CREATE TABLE IF NOT EXISTS span_collector.span (trace_id text, span_id text, name text, start_time double, finish_time double, PRIMARY KEY(trace_id, span_id));"
+TS="CREATE TABLE IF NOT EXISTS span_collector.span (trace_id text, span_id text, parent_id text, name text, start_time double, finish_time double, PRIMARY KEY(trace_id, span_id));"
 until echo $KS | cqlsh && echo $TS | cqlsh; do
     echo "cqlsh: Cassandra is unavailable - retry later"
     sleep 2
--- src/main.go
+++ src/main.go
@@ -14,6 +14,7 @@ import (
 type Span struct {
     TraceId string `json:"trace_id"`
     SpanId string `json:"span_id"`
+    ParentId string `json:"parent_id"`
     Name string `json:"name"`
     StartTime float64 `json:"start_time"`
     FinishTime float64 `json:"finish_time"`
@@ -27,8 +28,8 @@ func NewSpanCollector(session *gocql.Session) func(http.ResponseWriter, *http.Re
         query := "BEGIN BATCH "
         var values []interface{} = make([]interface{}, 0)
         for _, span := range incomingSpans {
-            query += "INSERT INTO span_collector.span (name, span_id, trace_id, start_time, finish_time) VALUES (?, ?, ?, ?, ?);"
-            values = append(values, span.Name, span.SpanId, span.TraceId, span.StartTime, span.FinishTime)
+            query += "INSERT INTO span_collector.span (name, span_id, parent_id, trace_id, start_time, finish_time) VALUES (?, ?, ?, ?, ?, ?);"
+            values = append(values, span.Name, span.SpanId, span.ParentId, span.TraceId, span.StartTime, span.FinishTime)
         }
         query += "APPLY BATCH;"
         log.Printf("From insert: %s", session.Query(query, values...).Exec())
@@ -37,18 +38,19 @@ func NewSpanCollector(session *gocql.Session) func(http.ResponseWriter, *http.Re
 
 func NewSpanViewer(session *gocql.Session) func(http.ResponseWriter, *http.Request) {
     return func(w http.ResponseWriter, r *http.Request) {
-        iter := session.Query("SELECT name, span_id, trace_id, start_time, finish_time FROM span_collector.span;").Iter()
+        iter := session.Query("SELECT name, span_id, trace_id, parent_id, start_time, finish_time FROM span_collector.span;").Iter()
         log.Print(iter)
         spans := make([]Span, 0)
 
         var name string
         var traceId string
         var spanId string
+        var parentId string
         var startTime float64
         var finishTime float64
 
-        for iter.Scan(&name, &spanId, &traceId, &startTime, &finishTime) {
-            spans = append(spans, Span{traceId, spanId, name, startTime, finishTime})
+        for iter.Scan(&name, &spanId, &traceId, &parentId, &startTime, &finishTime) {
+            spans = append(spans, Span{traceId, spanId, parentId, name, startTime, finishTime})
         }
 
         if err := iter.Close(); err != nil {
