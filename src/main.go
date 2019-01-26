e0491f5ab7bb2d5947a8e1235b28b6aeb140a75e
Bryan Clement <bclement@newrelic.com>
2019-01-25 19:20:56 -0800

    fmt'd

--- src/main.go
+++ src/main.go
@@ -14,17 +14,17 @@ import (
 )
 
 type Span struct {
-    TraceId    string            `cassandra:"trace_id" json:"trace_id"`
-    SpanId     string            `cassandra:"span_id" json:"span_id"`
-    ParentId   string            `cassandra:"parent_id" json:"parent_id"`
-    Name       string            `cassandra:"name" json:"name"`
-    StartTime  float64           `cassandra:"start_time" json:"start_time"`
-    FinishTime float64           `cassandra:"finish_time" json:"finish_time"`
-    Category   string            `cassandra:"category" json:"category"`
-    Tags       map[string]string `cassandra:"tags" json:"tags"`
-    LicenseKey string            `cassandra:"license_key" json:"license_key" query:"license_key"`
-    EntityName string            `cassandra:"entity_name" json:"entity_name" query:"entity_name"`
-    EntityId   string            `cassandra:"entity_id" json:"entity_id" query:"entity_id"`
+	TraceId    string            `cassandra:"trace_id" json:"trace_id"`
+	SpanId     string            `cassandra:"span_id" json:"span_id"`
+	ParentId   string            `cassandra:"parent_id" json:"parent_id"`
+	Name       string            `cassandra:"name" json:"name"`
+	StartTime  float64           `cassandra:"start_time" json:"start_time"`
+	FinishTime float64           `cassandra:"finish_time" json:"finish_time"`
+	Category   string            `cassandra:"category" json:"category"`
+	Tags       map[string]string `cassandra:"tags" json:"tags"`
+	LicenseKey string            `cassandra:"license_key" json:"license_key" query:"license_key"`
+	EntityName string            `cassandra:"entity_name" json:"entity_name" query:"entity_name"`
+	EntityId   string            `cassandra:"entity_id" json:"entity_id" query:"entity_id"`
 }
 
 func NewSpanCollector(session *gocql.Session) func(http.ResponseWriter, *http.Request) {
@@ -40,19 +40,19 @@ func NewSpanCollector(session *gocql.Session) func(http.ResponseWriter, *http.Re
 
 		query := "BEGIN BATCH "
 		for _, span := range incomingSpans {
-            spanType := reflect.TypeOf(span)
-            numFields := spanType.NumField()
+			spanType := reflect.TypeOf(span)
+			numFields := spanType.NumField()
 			fields := make([]string, numFields)
 			valuePlaceholders := make([]string, numFields)
 			for i := 0; i < numFields; i++ {
 				field := spanType.Field(i)
-                tag := field.Tag.Get("query")
-                if tag == "" {
-                    values = append(values, getField(&span, field.Name))
-                } else {
-                    values = append(values, queryParams[tag][0])
-                }
-                fields[i] = field.Tag.Get("cassandra")
+				tag := field.Tag.Get("query")
+				if tag == "" {
+					values = append(values, getField(&span, field.Name))
+				} else {
+					values = append(values, queryParams[tag][0])
+				}
+				fields[i] = field.Tag.Get("cassandra")
 				valuePlaceholders[i] = "?"
 			}
 
@@ -60,7 +60,7 @@ func NewSpanCollector(session *gocql.Session) func(http.ResponseWriter, *http.Re
 		}
 		query += "APPLY BATCH;"
 		log.Printf("The query is: %s", query)
-        log.Printf("The values are: %s", values)
+		log.Printf("The values are: %s", values)
 		log.Printf("The query params are %s", queryParams)
 		log.Printf("From insert: %s", session.Query(query, values...).Exec())
 	}
@@ -91,10 +91,10 @@ func NewSpanViewer(session *gocql.Session) func(http.ResponseWriter, *http.Reque
 			for i := 0; i < numFields; i++ {
 				field := spanType.Field(i)
 				tag := field.Tag.Get("cassandra")
-                val := reflect.ValueOf(row[tag])
-                if val.IsValid() {
-                    spanValue.FieldByName(field.Name).Set(val)
-                }
+				val := reflect.ValueOf(row[tag])
+				if val.IsValid() {
+					spanValue.FieldByName(field.Name).Set(val)
+				}
 			}
 
 			spans = append(spans, span)
