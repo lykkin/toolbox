8be2105cd83bc9a81bd00b654ecc54ff974448d4
Bryan Clement <bclement@newrelic.com>
2019-01-29 20:21:14 -0800

    processor works

--- processor/src/main.go
+++ processor/src/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
 	"time"
 
 	"shared"
@@ -15,11 +18,11 @@ type SpanEvent struct {
 	ParentId   string            `json:"parentId"`
 	Name       string            `json:"name"`
 	Timestamp  uint64            `json:"timestamp"`
-	Duration   float64           `json:"duration"`
-	Category   string            `json:"category"`
+	Duration   float64           `json:"durationMs"`
+	Category   string            `json:"-"` //category"`
 	Tags       map[string]string `json:"tags,omitempty"`
-	EntityName string            `json:"appName"`
-	EntityId   string            `json:"appId,omitempty"`
+	EntityName string            `json:"entityName"`
+	EntityId   string            `json:"entityId,omitempty"`
 }
 
 func SpanToEvent(s span.Span) SpanEvent {
@@ -43,8 +46,30 @@ type RequestResult struct {
 }
 
 func SendEvents(licenseKey string, events []SpanEvent, errChan chan RequestResult) {
-	//TODO: actually send the events
-	log.Printf("sending %s: %s", licenseKey, events)
+	payload := map[string][]SpanEvent{"spans": events}
+	body, err := json.Marshal(payload)
+	if err != nil {
+		errChan <- RequestResult{err, ""}
+		return
+	}
+
+	req, err := http.NewRequest("POST", "https://staging-collector.newrelic.com/agent_listener/invoke_raw_method", bytes.NewBuffer(body))
+	if err != nil {
+		errChan <- RequestResult{err, ""}
+		return
+	}
+	// set query params and headers
+	q := req.URL.Query()
+	q.Add("protocol_version", "1")
+	q.Add("license_key", licenseKey)
+	q.Add("method", "external_span_data")
+	req.Header.Set("Content-Type", "application/json")
+	req.URL.RawQuery = q.Encode()
+	client := &http.Client{}
+	res, err := client.Do(req)
+	defer res.Body.Close()
+
+	// TODO: parse response and propagate it to the main goroutine
 	errChan <- RequestResult{nil, licenseKey}
 }
 
@@ -57,12 +82,14 @@ func main() {
 		log.Fatal(err)
 	}
 	defer session.Close()
-	log.Print("Started processing!")
 	var HarvestPeriod time.Duration = 10 // In seconds
 	for {
+		// used to break events out into payloads to send
 		LicenseKeyToEvents := make(map[string][]SpanEvent)
+		// used to track unique trace ids to delete
 		LicenseKeyToTraceIds := make(map[string]map[string]bool)
 
+		// grab all spans to process
 		iter := session.Query("SELECT * FROM span_collector.to_process;").Iter()
 		for {
 			row := make(map[string]interface{})
@@ -72,10 +99,13 @@ func main() {
 
 			s := span.FromRow(row)
 			licenseKey := s.LicenseKey
-			if LicenseKeyToTraceIds[licenseKey] == nil {
+			// seed the trace id set for the license key if it doesn't exist
+			if _, ok := LicenseKeyToTraceIds[licenseKey]; !ok {
 				LicenseKeyToTraceIds[licenseKey] = make(map[string]bool)
 			}
+			// set the trace id as seen
 			LicenseKeyToTraceIds[licenseKey][s.TraceId] = true
+			// record the span in the payload to be setn
 			LicenseKeyToEvents[licenseKey] = append(LicenseKeyToEvents[licenseKey], SpanToEvent(s))
 		}
 
@@ -83,36 +113,46 @@ func main() {
 			log.Fatal(err)
 		}
 
-		numRequestsAwaiting := 0
-		comms := make(chan RequestResult)
-		for licenseKey, events := range LicenseKeyToEvents {
-			go SendEvents(licenseKey, events, comms)
-			numRequestsAwaiting++
-		}
-
-		query := "BEGIN BATCH "
-		values := make([]interface{}, 0)
-		for ; numRequestsAwaiting != 0; numRequestsAwaiting-- {
-			result := <-comms
-			if result.Err != nil {
-				log.Printf("ran into an error while sending events: %s", result.Err)
-				continue
+		// only process if there are events to send
+		if len(LicenseKeyToEvents) > 0 {
+			numRequestsAwaiting := 0
+			comms := make(chan RequestResult)
+			// loop through the events bucketed by license key and kick the request off in parallel
+			for licenseKey, events := range LicenseKeyToEvents {
+				go SendEvents(licenseKey, events, comms)
+				// record the number of outstanding requests
+				numRequestsAwaiting++
 			}
 
-			// Build query to mark events as seen
-			licenseKey := result.LicenseKey
-			for traceId, _ := range LicenseKeyToTraceIds[licenseKey] {
-				query += "DELETE FROM span_collector.to_process WHERE trace_id=?;"
-				values = append(values, traceId)
-			}
-		}
+			query := "BEGIN BATCH "
+			values := make([]interface{}, 0)
+			// read off the return channel till all requests have come back
+			for ; numRequestsAwaiting != 0; numRequestsAwaiting-- {
+				result := <-comms
+				// if there was an error, don't remove the events from
+				// the queue, and let next pass pick them up.
 
-		query += "APPLY BATCH;"
-		log.Printf("EXECUTING QUERY: %s", query)
-		log.Printf("WITH VALUES: %s", values)
-		log.Printf("FROM DELETE: %s", session.Query(query, values...).Exec())
+				// TODO: extra error handling here? are there cases where we want to throw the events away anyway?
+				if result.Err != nil {
+					continue
+				}
+
+				// delete the processed spans from the queue
+				licenseKey := result.LicenseKey
+				for traceId, _ := range LicenseKeyToTraceIds[licenseKey] {
+					query += "DELETE FROM span_collector.to_process WHERE trace_id=? AND license_key=?;"
+					values = append(values, traceId, licenseKey)
+				}
+			}
+
+			query += "APPLY BATCH;"
+			log.Printf("EXECUTING QUERY: %s", query)
+			log.Printf("WITH VALUES: %s", values)
+			log.Printf("FROM DELETE: %s", session.Query(query, values...).Exec())
+		}
 
 		log.Print("waiting")
+		// wait some time for more spans to roll in
 		timer1 := time.NewTimer(HarvestPeriod * time.Second)
 		<-timer1.C
 	}
